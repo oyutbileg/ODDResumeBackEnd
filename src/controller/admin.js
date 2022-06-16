5006f50dfa9d34af9a8cc91217662e0ca047b55d
Erdenebulgan Sainkhishig <erdenebulgan.s@mobicom.mn>
2022-06-16 13:27:09 +0800

    add change site password

--- src/controller/admin.js
+++ src/controller/admin.js
@@ -3,6 +3,7 @@ const asyncHandler = require("express-async-handler");
 const Sequelize = require("sequelize");
 const responseHandler = require("../utils/responseHandler");
 const Op = Sequelize.Op;
+const bcrypt = require("bcrypt");
 
 exports.adminRole = asyncHandler(async (req, res, _next) => {
   const user = await req.db.sysUser.findOne({
@@ -51,6 +52,36 @@ exports.deleteUser = asyncHandler(async (req, res, _next) => {
 
 exports.createConfig = asyncHandler(async (req, res, _next) => {
   await req.db.config.create(req.body);
+  responseHandler(res, {
+    data: {}
+  })
+});
+
+exports.changeConfigPassword = asyncHandler(async (req, res, _next) => {
+  const config = await req.db.config.findByPk(1);
+
+  if (!config) {
+    throw new MyError("Config not found",
+      404
+    );
+  }
+
+  if (!req.body.site_password && !req.body.old_password) {
+    throw new MyError("Please enter a password!",
+      404
+    );
+  }
+
+  const ok = await bcrypt.compare(req.body.old_password, config.site_password);
+
+  if (!ok) {
+    throw new MyError("!Oops, old password don't match.", 401);
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  config.site_password = await bcrypt.hash(req.body.site_password, salt);
+  await config.save();
+
   responseHandler(res, {
     data: {}
   })
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { route } = require('express/lib/application');
-const { adminRole, deleteUser, createConfig } = require('../controller/admin');
+const { adminRole, deleteUser, createConfig, changeConfigPassword } = require('../controller/admin');
 const { protect, authorize } = require('../middleware/protect');
 
 const router = express.Router();
@@ -10,6 +10,7 @@ router.use(protect);
 router.use(authorize());
 router.route('/is-admin/:id').put(adminRole);
 router.route('/config').post(createConfig);
+router.route('/config/change-password').post(changeConfigPassword);
 router.route('/:id').delete(deleteUser);
 
 module.exports = router;
