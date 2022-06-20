4adc48129cbcfe05210fe9f33c2c41e507bed3b3
Erdenebulgan Sainkhishig <erdenebulgan.s@mobicom.mn>
2022-06-20 12:24:35 +0800

    add change order route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { protect, authorize } = require('../middleware/protect');
-const { me, changePassword, update, uploadPhoto } = require("../controller/user");
+const { me, changePassword, update, uploadPhoto, changeOrder } = require("../controller/user");
 
 const router = express.Router();
 
@@ -8,7 +8,8 @@ const router = express.Router();
 router.use(protect);
 router.route("/").put(update);
 router.route("/me").get(me);
-router.route("/change-password").post(changePassword);
 router.route("/upload-photo").put(uploadPhoto);
+router.route("/change-password").post(changePassword);
+router.route('/change-order').post(authorize(), changeOrder);
 
 module.exports = router;
