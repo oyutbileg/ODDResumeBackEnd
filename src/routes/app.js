7f39aaf71226200047f8706f46a851c7a3386cb1
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2022-10-07 14:29:27 +0800

    remove authentication temp

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const { alive, getUsers, getPortfolio } = require('../controller/app');
-const { siteProtect } = require('../middleware/protect');
+const express = require("express");
+const { alive, getUsers, getPortfolio } = require("../controller/app");
+const { siteProtect } = require("../middleware/protect");
 const router = express.Router();
 // api/v1
-router.route('/alive').get(alive);
-router.route('/users').get(siteProtect, getUsers);
-router.route('/portfolio/:portfolioId/').get(siteProtect, getPortfolio);
+// siteProtect
+router.route("/alive").get(alive);
+router.route("/users").get(getUsers);
+router.route("/portfolio/:portfolioId/").get(getPortfolio);
 
 module.exports = router;
